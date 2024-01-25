f410cf76ac5ba397a15ce5b0cc98cb25d40ff3a5
24499528 <157749114+24499528@users.noreply.github.com>
2024-01-26 04:05:19 +0800

    Add files via upload

---
+++ PalServerInstall.sh
@@ -0,0 +1,299 @@
+#!/bin/bash
+# 检查 jq 是否已经安装
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'jq 没有安装，正在尝试安装...'
+  sudo apt-get update
+  sudo apt-get install -y jq
+fi
+# 当前的脚本和游戏版本
+currentScriptVersion="0.1.3"
+currentGameVersion="1.3"
+
+# 从服务器获取版本信息
+versionInfo=$(curl -s https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/version.json)
+
+# 解析JSON以获取最新的脚本和游戏版本
+latestScriptVersion=$(echo $versionInfo | jq -r '.scriptVersion')
+latestGameVersion=$(echo $versionInfo | jq -r '.gameVersion')
+
+# 比较脚本版本
+if [[ $(echo -e "$currentScriptVersion\n$latestScriptVersion" | sort -V | head -n 1) != $latestScriptVersion ]]; then
+    echo "新的脚本版本可用，你的版本为 $currentScriptVersion，最新版本为 $latestScriptVersion。正在下载新版本..."
+    # 下载新版本的脚本
+    curl -O https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/PalServerInstall.sh
+    chmod +x PalServerInstall.sh
+    exit
+fi
+
+# 比较游戏版本
+if [[ $(echo -e "$currentGameVersion\n$latestGameVersion" | sort -V | head -n 1) != $latestGameVersion ]]; then
+    echo "新的游戏版本可用，你的版本为 $currentGameVersion，最新版本为 $latestGameVersion。请升级。"
+fi
+#!/usr/bin/env bash
+#Blog:https://www.xuehaiwu.com/
+
+Green="\033[32m"
+Font="\033[0m"
+Red="\033[31m" 
+
+#root权限
+root_need(){
+    if [[ $EUID -ne 0 ]]; then
+        echo -e "${Red}Error:This script must be run as root!${Font}"
+        exit 1
+    fi
+}
+
+#检测ovz
+ovz_no(){
+    if [[ -d "/proc/vz" ]]; then
+        echo -e "${Red}Your VPS is based on OpenVZ，not supported!${Font}"
+        exit 1
+    fi
+}
+
+#检测并安装Docker
+install_docker(){
+    if command -v docker &> /dev/null; then
+        echo -e "${Green}Docker 已安装，进行下一步.${Font}"
+    else
+        echo -e "${Green}Docker 未安装，正在为您安装...${Font}"
+        curl -fsSL https://get.docker.com | bash -s docker
+        echo -e "${Green}Docker 安装成功！${Font}"
+    fi
+}
+
+#安装幻兽帕鲁服务端
+install_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Red}幻兽帕鲁服务端已存在，安装失败！${Font}"
+    else
+        echo -e "${Green}开始安装幻兽帕鲁服务端...${Font}"
+        CONTAINER_ID=$(docker run -dit --name steamcmd --net host cm2network/steamcmd)
+        docker exec -it $CONTAINER_ID bash -c "/home/steam/steamcmd/steamcmd.sh +login anonymous +app_update 2394010 validate +quit"
+        wget -O restart.sh https://www.xuehaiwu.com/wp-content/uploads/shell/Pal/restart.sh --no-check-certificate &&chmod +x restart.sh 
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功安装并启动！${Font}"
+    fi
+}
+
+#启动幻兽帕鲁服务端
+start_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始启动幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
+    fi
+}
+#更新幻兽帕鲁服务端
+update_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始启动幻兽帕鲁服务端...${Font}"
+        docker exec -it steamcmd /bin/bash -c "/home/steam/steamcmd/steamcmd.sh +login anonymous +app_update 2394010 validate +quit"
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功启动！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，启动失败！${Font}"
+    fi
+}
+#停止幻兽帕鲁服务端
+stop_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始停止幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功停止！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，停止失败！${Font}"
+    fi
+}
+#查看幻兽帕鲁服务端状态
+check_pal_server_status(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}幻兽帕鲁服务端状态如下：${Font}"
+        docker ps -a -f name=steamcmd
+        echo -e "${Green}幻兽帕鲁服务端资源使用情况如下：${Font}"
+        docker stats --no-stream steamcmd
+        echo -e "${Green}服务器内存使用情况如下：${Font}"
+        free -h
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在！${Font}"
+    fi
+}
+#修改服务端配置
+modify_config(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        if [ -f ./PalWorldSettings.ini ]; then
+            echo -e "${Green}开始修改服务端配置...${Font}"
+            docker cp ./PalWorldSettings.ini steamcmd:/home/steam/Steam/steamapps/common/PalServer/Pal/Saved/Config/LinuxServer/
+            echo -e "${Green}服务端配置已成功修改！服务端重启后生效！${Font}"
+        else
+            echo -e "${Red}未找到服务端配置文件，请前往https://www.xuehaiwu.com/Pal/进行下载。${Font}"
+        fi
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，修改配置失败！${Font}"
+    fi
+}
+
+#增加swap内存
+add_swap(){
+echo -e "${Green}请输入需要添加的swap，建议为内存的2倍！${Font}"
+read -p "请输入swap数值单位MB:" swapsize
+
+#检查是否存在swapfile
+grep -q "swapfile" /etc/fstab
+
+#如果不存在将为其创建swap
+if [ $? -ne 0 ]; then
+    echo -e "${Green}swapfile未发现，正在为其创建swapfile${Font}"
+    fallocate -l ${swapsize}M /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+         echo -e "${Green}swap创建成功，并查看信息：${Font}"
+         cat /proc/swaps
+         cat /proc/meminfo | grep Swap
+else
+    echo -e "${Red}swapfile已存在，swap设置失败，请先运行脚本删除swap后重新设置！${Font}"
+fi
+}
+del_swap(){
+#检查是否存在swapfile
+grep -q "swapfile" /etc/fstab
+
+#如果存在就将其移除
+if [ $? -eq 0 ]; then
+	echo -e "${Green}swapfile已发现，正在将其移除...${Font}"
+	sed -i '/swapfile/d' /etc/fstab
+	echo "3" > /proc/sys/vm/drop_caches
+	swapoff -a
+	rm -f /swapfile
+    echo -e "${Green}swap已删除！${Font}"
+else
+	echo -e "${Red}swapfile未发现，swap删除失败！${Font}"
+fi
+}
+#增加定时重启
+add_restart(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始增加定时重启...${Font}"
+        echo -e "${Green}1、每天凌晨5点${Font}"
+        echo -e "${Green}2、每周三凌晨5点${Font}"
+        echo -e "${Green}3、自定义${Font}"
+        read -p "请输入数字 [1-3]:" num
+        case "$num" in
+            1)
+            echo "0 5 * * * cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            2)
+            echo "0 5 * * 3 cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            3)
+            read -p "请输入定时重启的cron表达式:" cron
+            echo "$cron cd $(pwd) && ./restart.sh" >> /etc/crontab
+            ;;
+            *)
+            echo -e "${Red}请输入正确数字 [1-3]${Font}"
+            add_restart
+            ;;
+        esac
+        echo -e "${Green}定时重启已成功增加！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，增加定时重启失败！${Font}"
+    fi
+}
+
+#重启幻兽帕鲁服务端
+restart_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始重启幻兽帕鲁服务端...${Font}"
+        docker start steamcmd
+        ./restart.sh
+        echo -e "${Green}幻兽帕鲁服务端已成功重启！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，重启失败！${Font}"
+    fi
+}
+
+#删除幻兽帕鲁服务端
+delete_pal_server(){
+    if [ $(docker ps -a -q -f name=steamcmd) ]; then
+        echo -e "${Green}开始删除幻兽帕鲁服务端...${Font}"
+        docker stop steamcmd
+        docker rm steamcmd
+        echo -e "${Green}幻兽帕鲁服务端已成功删除！${Font}"
+    else
+        echo -e "${Red}幻兽帕鲁服务端不存在，删除失败！${Font}"
+    fi
+}
+
+#开始菜单
+main(){
+root_need
+ovz_no
+install_docker
+clear
+echo -e "———————————————————————————————————————"
+echo -e "${Green}Linux VPS一键安装管理幻兽帕鲁服务端脚本${Font}"
+echo -e "${Green}脚本版本${currentScriptVersion}${Font}"
+echo -e "${Green}教程地址：QQ:24499528${Font}"
+echo -e "${Green}1、安装幻兽帕鲁服务端${Font}"
+echo -e "${Green}2、启动幻兽帕鲁服务端${Font}"
+echo -e "${Green}3、停止幻兽帕鲁服务端${Font}"
+echo -e "${Green}4、修改服务端配置${Font}"
+echo -e "${Green}5、增加swap内存${Font}"
+echo -e "${Green}6、删除swap内存${Font}"
+echo -e "${Green}7、增加定时重启${Font}"
+echo -e "${Green}8、重启幻兽帕鲁服务端${Font}"
+echo -e "${Green}9、查看幻兽帕鲁服务端状态${Font}"
+echo -e "${Green}10、更新幻兽帕鲁服务端${Font}"
+echo -e "${Green}11、删除幻兽帕鲁服务端${Font}"
+echo -e "———————————————————————————————————————"
+read -p "请输入数字 [1-11]:" num
+case "$num" in
+    1)
+    install_pal_server
+    ;;
+    2)
+    start_pal_server
+    ;;
+    3)
+    stop_pal_server
+    ;;
+    4)
+    modify_config
+    ;;
+    5)
+    add_swap
+    ;;
+    6)
+    del_swap
+    ;;
+    7)
+    add_restart
+    ;;
+    8)
+    restart_pal_server
+    ;;
+   9)
+    check_pal_server_status
+    ;;
+	10)
+    update_pal_server
+    ;;
+    11)
+    delete_pal_server
+    ;;
+    *)
+	
+    clear
+    echo -e "${Green}请输入正确数字 [1-11]${Font}"
+    sleep 2s
+    main
+    ;;
+    esac
+}
+main
---
+++ README.md
@@ -0,0 +1,25 @@
+# 🎮 Pal-Server-Install
+
+这是一个用于安装和管理 幻兽帕鲁服务端 的脚本。
+
+🔗 **教程地址**：
+[https://www.xuehaiwu.com/palworld-server/](https://www.xuehaiwu.com/palworld-server/)
+
+🛠️ **在线生成服务器配置**：
+[https://www.xuehaiwu.com/Pal/](https://www.xuehaiwu.com/Pal/)
+
+## 📝 更新日志：
+
+**2024.01.25**
+- 🗑️ 增加删除 swap 功能
+- 🕵️ 增加游戏版本号检测
+- 🔄 增加脚本版本号检测更新
+- 🔧 增加游戏服务端更新
+
+## 🚀 快速开始
+
+在你的服务器上运行以下命令来安装和启动 幻兽帕鲁服务端：
+
+```bash
+wget -O PalServerInstall.sh https://mirror.ghproxy.com/https://raw.githubusercontent.com/2lifetop/Pal-Server-Install/main/PalServerInstall.sh && chmod +x PalServerInstall.sh && ./PalServerInstall.sh
+```
